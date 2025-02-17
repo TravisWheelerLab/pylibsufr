c5537bb5005684f62c5516112aaeced5a1d62e90
George Glidden-Handgis <47768122+georgeglidden@users.noreply.github.com>
2025-02-17 16:11:39 -0700

    wraps CountOptions, CountResults, and implements SuffixArray.count

--- pylibsufr/__init__.py
+++ pylibsufr/__init__.py
@@ -1,2 +1,28 @@
 import _pylibsufr
-print(_pylibsufr.PySuffixArray)
+
+def test_create_and_count(input_file: str, output_file: str, queries: list[str]):
+    sequence_file_data = _pylibsufr.py_read_sequence_file(
+        input_file, 
+        ord('%'))
+    sufr_builder_args = _pylibsufr.PySufrBuilderArgs(
+        sequence_file_data.seq(), 
+        output_file, 
+        False, 
+        None, 
+        True, 
+        False, 
+        False, 
+        sequence_file_data.start_positions(), 
+        sequence_file_data.sequence_names(), 
+        16, 
+        None, 
+        42)
+    suffix_array = _pylibsufr.PySuffixArray(sufr_builder_args)
+    count_options = _pylibsufr.PyCountOptions(
+        queries,
+        None,
+        False
+    )
+    count_results = suffix_array.count(count_options)
+    for res in count_results:
+        print(res.query_num, res.query, res.count)
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 use libsufr::{
     suffix_array::SuffixArray,
     types::{
-        SufrBuilderArgs, SequenceFileData,
+        SufrBuilderArgs, SequenceFileData, CountOptions, CountResult
     },
     util::read_sequence_file,
 };
@@ -15,15 +15,6 @@ pub struct PySequenceFileData {
 
 #[pymethods]
 impl PySequenceFileData {
-    // #[new]
-    // fn new(
-    //     sequence_file_data: SequenceFileData
-    // ) -> PyResult<PySequenceFileData> {
-    //     Ok(PySequenceFileData {
-    //         sequence_file_data: sequence_file_data
-    //     })
-    // }
-    
     fn seq(&self) -> PyResult<Vec<u8>> {
         Ok(self.sequence_file_data.seq.clone())
     }
@@ -92,6 +83,57 @@ impl PySufrBuilderArgs {
 }
 
 #[pyclass]
+pub struct PyCountOptions {
+    count_options: CountOptions
+}
+
+impl Clone for PyCountOptions {
+    fn clone(&self) -> Self {
+        PyCountOptions {
+            count_options: CountOptions {
+                queries: self.count_options.queries.clone(),
+                max_query_len: self.count_options.max_query_len.clone(),
+                low_memory: self.count_options.low_memory.clone(),
+            }
+        }
+    }
+}
+
+#[pymethods]
+impl PyCountOptions {
+    #[new]
+    #[pyo3(signature = (queries, max_query_len, low_memory))]
+    pub fn new(
+        queries: Vec<String>, 
+        max_query_len: Option<usize>,
+        low_memory: bool,
+    ) -> PyResult<PyCountOptions> {
+        Ok(PyCountOptions {
+            count_options: CountOptions {
+                queries: queries,
+                max_query_len: max_query_len,
+                low_memory: low_memory,
+            }
+        })
+    }
+}
+
+#[pyclass]
+pub struct PyCountResult {
+    /// The ordinal position of the original query
+    #[pyo3(get)]
+    pub query_num: usize,
+
+    /// The query string
+    #[pyo3(get)]
+    pub query: String,
+
+    /// Number of times a query was found
+    #[pyo3(get)]
+    pub count: usize,
+}
+
+#[pyclass]
 pub struct PySuffixArray {
     suffix_array: SuffixArray
 }
@@ -99,33 +141,37 @@ pub struct PySuffixArray {
 unsafe impl Send for PySuffixArray {}
 unsafe impl Sync for PySuffixArray {}
 
+
+
 #[pymethods]
 impl PySuffixArray {
     #[new]
     pub fn new(args: PySufrBuilderArgs) -> PyResult<PySuffixArray> {
         let low_memory = args.sufr_builder_args.low_memory;
         let path = SuffixArray::write(args.sufr_builder_args).unwrap();
-        //SuffixArray::read(&path, low_memory).unwrap()
+        Self::read(path, low_memory)
+        //Ok(PySuffixArray {
+        //    suffix_array: SuffixArray::read(&path, low_memory).unwrap()
+        //})
+    }
+    #[staticmethod]
+    pub fn read(filename: String, low_memory: bool) -> PyResult<PySuffixArray> {
         Ok(PySuffixArray {
-            suffix_array: SuffixArray::read(&path, low_memory).unwrap()
+            suffix_array: SuffixArray::read(&filename, low_memory).unwrap()
         })
     }
-    // pub fn write(args: PySufrBuilderArgs) -> PyResult<String> {
-    //     Ok(SuffixArray::write(args.sufr_builder_args).unwrap())
-    // }
-    // pub fn read(filename: &str, low_memory: bool) -> PyResult<PySuffixArray> {
-    //     Ok(PySuffixArray { 
-    //         suffix_array: SuffixArray::read(filename, low_memory).unwrap()
-    //     })
-    // }
-    // pub fn count(&mut self, args: PyCountOptions) -> PyResult<Vec<PyCountResult>> {
-    //     Ok(self.suffix_array.count(args.count_options)
-    //         .unwrap()
-    //         .iter()
-    //         .map(|cr| PyCountResult{ count_result: cr})
-    //         .collect()
-    //     )
-    // }
+    pub fn count(&mut self, args: PyCountOptions) -> PyResult<Vec<PyCountResult>> {
+        Ok(self.suffix_array.count(args.count_options)
+            .unwrap()
+            .iter()
+            .map(|count_result| PyCountResult {
+                query_num: count_result.query_num.clone(),
+                query: count_result.query.clone(),
+                count: count_result.count.clone(),
+            })
+            .collect()
+        )
+    }
     // pub fn extract(&mut self, args: PyExtractOptions) -> PyResult<Vec<PyExtractResult>> {
     //     Ok(self.suffix_array.extract(args.extract_options).unwrap())
     // }
@@ -147,6 +193,8 @@ impl PySuffixArray {
 #[pymodule]
 fn _pylibsufr(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(py_read_sequence_file, m)?)?;
+    m.add_class::<PyCountResult>()?;
+    m.add_class::<PyCountOptions>()?;
     m.add_class::<PySufrBuilderArgs>()?;
     m.add_class::<PySuffixArray>()?;
     Ok(())
@@ -193,10 +241,7 @@ fn _pylibsufr(m: &Bound<'_, PyModule>) -> PyResult<()> {
 //    Ok(ret)
 //}
 
-//#[pyclass]
-//pub struct PyCountOptions {
-//    count_options: CountOptions
-//}
+
 // 
 // #[pyclass]
 // pub struct PyExtractOptions {
@@ -211,11 +256,6 @@ fn _pylibsufr(m: &Bound<'_, PyModule>) -> PyResult<()> {
 // #[pyclass]
 // pub struct PyLocateOptions {
 //     locate_options: LocateOptions
-// }
-// 
-// #[pyclass]
-// pub struct PyCountResult {
-//     count_result: CountResult
 // }
 // 
 // #[pyclass]
