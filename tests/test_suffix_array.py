5b084d9cd153ec9f5305f48ed67083a703d5fbe4
George Glidden-Handgis <47768122+georgeglidden@users.noreply.github.com>
2025-03-06 15:02:10 -0700

    implemented scaffolding for unit tests

---
+++ tests/test_suffix_array.py
@@ -0,0 +1,35 @@
+import unittest
+
+class TestSuffixArray(unittest.TestCase):
+
+    def test_create(self):
+        pass
+
+    def test_count(self):
+        pass
+
+    def test_extract(self):
+        pass
+
+    def test_list(self):
+        pass
+
+    def test_locate(self):
+        pass
+
+    def test_metadta(self):
+        pass
+
+    def test_read(self):
+        pass
+
+    def test_string_at(self):
+        """deprecated."""
+        pass
+        
+    def test_write(self):
+        """to be implemented."""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_util.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestUtil(unittest.TestCase):
+
+    def test_read_sequence_file(self):
+        pass
+
+    def test_read_text_length(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
